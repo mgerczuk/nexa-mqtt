c1d4bb9e8513bc7db50954f95c644e624996132a
Markus Troßbach <markus@trossbach.net>
2025-01-13 15:27:23 +0100

    Allow to set charging_limit and discharge_limit independent

--- internal/polling/parameters.go
+++ internal/polling/parameters.go
@@ -16,27 +16,54 @@ func (s *Service) parametersSubscription(sn string) func(client mqtt.Client, mes
 		}
 
 		if payload.OutputPower != nil {
-			slog.Info("Trying to set default power", slog.String("device", sn), slog.Int("power", int(*payload.OutputPower)))
-			if err := s.options.GrowattClient.SetDefaultPower(sn, *payload.OutputPower); err != nil {
-				slog.Error("unable to set default power", slog.String("error", err.Error()), slog.String("device", sn))
-			} else {
-				changedSomething = true
-				slog.Info("set default power", slog.String("device", sn), slog.Int("power", int(*payload.OutputPower)))
-			}
+			changedSomething = changedSomething || s.setParameterOutputPower(sn, *payload.OutputPower)
 		}
 
-		if payload.ChargingLimit != nil && payload.DischargeLimit != nil {
-			slog.Info("Trying to set charging/discharge limit", slog.String("device", sn), slog.Float64("chargingLimit", *payload.ChargingLimit), slog.Float64("dischargeLimit", *payload.DischargeLimit))
-			if err := s.options.GrowattClient.SetSocLimit(sn, *payload.ChargingLimit, *payload.DischargeLimit); err != nil {
-				slog.Error("unable to set charging/discharge limit", slog.String("error", err.Error()))
-			} else {
-				changedSomething = true
-				slog.Info("set charging/discharge limit", slog.String("device", sn), slog.Float64("chargingLimit", *payload.ChargingLimit), slog.Float64("dischargeLimit", *payload.DischargeLimit))
-			}
+		if payload.ChargingLimit != nil || payload.DischargeLimit != nil {
+			changedSomething = changedSomething || s.setParameterChargingLimits(sn, payload.ChargingLimit, payload.DischargeLimit)
 		}
 
 		if changedSomething {
 			s.pollParameterData(sn)
 		}
+	}
+}
+
+func (s *Service) setParameterOutputPower(sn string, outputPower float64) bool {
+	slog.Info("trying to set default power", slog.String("device", sn), slog.Int("power", int(outputPower)))
+	if err := s.options.GrowattClient.SetDefaultPower(sn, outputPower); err != nil {
+		slog.Error("unable to set default power", slog.String("error", err.Error()), slog.String("device", sn))
+		return false
+	} else {
+		slog.Info("set default power", slog.String("device", sn), slog.Int("power", int(outputPower)))
+		return true
+	}
+}
+
+func (s *Service) setParameterChargingLimits(sn string, chargingLimit *float64, dischargeLimit *float64) bool {
+	if chargingLimit == nil || dischargeLimit == nil {
+		slog.Info("charging limit or discharge limit value not provided, trying to fetch them from server", slog.String("device", sn))
+		if data, err := s.options.GrowattClient.GetNoahInfo(sn); err != nil {
+			slog.Error("unable to get missing charging limit / discharge limit parameter", slog.String("error", err.Error()))
+			return false
+		} else {
+			if chargingLimit == nil {
+				cl := parseFloat(data.Obj.Noah.ChargingSocHighLimit)
+				chargingLimit = &cl
+			}
+			if dischargeLimit == nil {
+				dl := parseFloat(data.Obj.Noah.ChargingSocLowLimit)
+				dischargeLimit = &dl
+			}
+		}
+	}
+
+	slog.Info("trying to set charging/discharge limit", slog.String("device", sn), slog.Float64("chargingLimit", *chargingLimit), slog.Float64("dischargeLimit", *dischargeLimit))
+	if err := s.options.GrowattClient.SetSocLimit(sn, *chargingLimit, *dischargeLimit); err != nil {
+		slog.Error("unable to set charging/discharge limit", slog.String("error", err.Error()))
+		return false
+	} else {
+		slog.Info("set charging/discharge limit", slog.String("device", sn), slog.Float64("chargingLimit", *chargingLimit), slog.Float64("dischargeLimit", *dischargeLimit))
+		return true
 	}
 }
