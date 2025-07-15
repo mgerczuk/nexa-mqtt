8ae7427a7cca81d5912eacebb01c549da53ea010
Martin <martin@gerczuk.de>
2025-07-15 13:37:08 +0200

    Unit test package growatt_web (client.go)

--- internal/growatt_web/client_test.go
+++ internal/growatt_web/client_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/cookiejar"
 	"net/url"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -67,7 +68,7 @@ func Test_postForm_RetryLogin(t *testing.T) {
 		nil,
 	).Return(errors.New("invalid character '<' looking for beginning of value")).Once()
 
-	mockHttpClient.OnLogin("user", "secret", nil)
+	mockHttpClient.OnLogin("user", "secret", GrowattResult{}, nil)
 
 	mockHttpClient.On(
 		"postForm",
@@ -92,7 +93,7 @@ func Test_postForm_RetryLoginFails(t *testing.T) {
 		nil,
 	).Return(errors.New("invalid character '<' looking for beginning of value"))
 
-	mockHttpClient.OnLogin("user", "secret", errors.New("login failed"))
+	mockHttpClient.OnLogin("user", "secret", GrowattResult{}, errors.New("login failed"))
 
 	defer func() {
 		if r := recover(); r != nil {
@@ -107,17 +108,27 @@ func Test_postForm_RetryLoginFails(t *testing.T) {
 func TestLogin_Ok(t *testing.T) {
 	mockHttpClient, client := setupMocks(t)
 
-	mockHttpClient.OnLogin("user", "secret", nil)
+	mockHttpClient.OnLogin("user", "secret", GrowattResult{}, nil)
 
 	err := client.Login()
 
 	assert.Nil(t, err)
 }
 
+func TestLogin_ErrResult(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	mockHttpClient.OnLogin("user", "secret", GrowattResult{Result: -1}, nil)
+
+	err := client.Login()
+
+	assert.NotNil(t, err)
+}
+
 func TestLogin_Fails(t *testing.T) {
 	mockHttpClient, client := setupMocks(t)
 
-	mockHttpClient.OnLogin("user", "secret", errors.New("login fails"))
+	mockHttpClient.OnLogin("user", "secret", GrowattResult{}, errors.New("login fails"))
 
 	err := client.Login()
 
@@ -147,6 +158,150 @@ func TestGetPlantList_Fail(t *testing.T) {
 	mockHttpClient.OnGetPlantList(expected, errors.New("GetPlantList fails"))
 
 	result, err := client.GetPlantList()
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+}
+
+func TestGetPlantDevices_Ok(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	mockHttpClient.OnGetPlantDevices("pid", GrowattPlantDevices{}, nil)
+
+	result, err := client.GetPlantDevices("pid")
+
+	assert.NotNil(t, result)
+	assert.Nil(t, err)
+}
+
+func TestGetPlantDevices_Fail(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	mockHttpClient.OnGetPlantDevices("pid", GrowattPlantDevices{}, errors.New("GetPlantDevices fails"))
+
+	result, err := client.GetPlantDevices("pid")
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+}
+
+func TestGetNoahList_Ok(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	mockHttpClient.OnGetNoahList(12, GrowattNoahList{}, nil)
+
+	result, err := client.GetNoahList(12)
+
+	assert.NotNil(t, result)
+	assert.Nil(t, err)
+}
+
+func TestGetNoahList_Fail(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	mockHttpClient.OnGetNoahList(12, GrowattNoahList{}, errors.New("GetNoahList fails"))
+
+	result, err := client.GetNoahList(12)
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+}
+
+func TestGetNoahDetails_Ok(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	mockHttpClient.OnGetNoahDetails(12, "Serial123", GrowattNoahList{}, nil)
+
+	result, err := client.GetNoahDetails(12, "Serial123")
+
+	assert.NotNil(t, result)
+	assert.Nil(t, err)
+}
+
+func TestGetNoahDetails_Fail(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	mockHttpClient.OnGetNoahDetails(12, "Serial123", GrowattNoahList{}, errors.New("GetNoahDetails fails"))
+
+	result, err := client.GetNoahDetails(12, "Serial123")
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+}
+
+func TestGetNoahHistory_Ok(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	mockHttpClient.OnGetNoahHistory("Serial123", "2025-07-01", "2025-07-02", GrowattNoahHistory{}, nil)
+
+	result, err := client.GetNoahHistory("Serial123", "2025-07-01", "2025-07-02")
+
+	assert.NotNil(t, result)
+	assert.Nil(t, err)
+}
+
+func TestGetNoahHistory_EmptyDate(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	expected := time.Now().Format("2006-01-02")
+	mockHttpClient.OnGetNoahHistory("Serial123", expected, expected, GrowattNoahHistory{}, nil)
+
+	result, err := client.GetNoahHistory("Serial123", "", "")
+
+	assert.NotNil(t, result)
+	assert.Nil(t, err)
+}
+
+func TestGetNoahHistory_Fail(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	mockHttpClient.OnGetNoahHistory("Serial123", "2025-07-01", "2025-07-02", GrowattNoahHistory{}, errors.New("GetNoahHistory fails"))
+
+	result, err := client.GetNoahHistory("Serial123", "2025-07-01", "2025-07-02")
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+}
+
+func TestGetNoahStatus_Ok(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	mockHttpClient.OnGetNoahStatus(42, "Serial123", GrowattNoahStatus{}, nil)
+
+	result, err := client.GetNoahStatus(42, "Serial123")
+
+	assert.NotNil(t, result)
+	assert.Nil(t, err)
+}
+
+func TestGetNoahStatus_Fail(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	mockHttpClient.OnGetNoahStatus(42, "Serial123", GrowattNoahStatus{}, errors.New("GetNoahStatus fails"))
+
+	result, err := client.GetNoahStatus(42, "Serial123")
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+}
+
+func TestGetNoahTotals_Ok(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	mockHttpClient.OnGetNoahTotals(42, "Serial123", GrowattNoahTotals{}, nil)
+
+	result, err := client.GetNoahTotals(42, "Serial123")
+
+	assert.NotNil(t, result)
+	assert.Nil(t, err)
+}
+
+func TestGetNoahTotals_Fail(t *testing.T) {
+	mockHttpClient, client := setupMocks(t)
+
+	mockHttpClient.OnGetNoahTotals(42, "Serial123", GrowattNoahTotals{}, errors.New("GetNoahTotals fails"))
+
+	result, err := client.GetNoahTotals(42, "Serial123")
 
 	assert.Nil(t, result)
 	assert.NotNil(t, err)
--- internal/growatt_web/mockhttpclient_test.go
+++ internal/growatt_web/mockhttpclient_test.go
@@ -1,6 +1,7 @@
 package growatt_web
 
 import (
+	"fmt"
 	"net/url"
 
 	"github.com/stretchr/testify/mock"
@@ -20,7 +21,7 @@ func (h *MockHttpClient) postForm(url string, data url.Values, responseBody any)
 
 // ----- Helpers ------------------------------------------------------------
 
-func (m *MockHttpClient) OnLogin(account string, password string, err error) *mock.Call {
+func (m *MockHttpClient) OnLogin(account string, password string, result GrowattResult, err error) *mock.Call {
 	call := m.On(
 		"postForm",
 		"https://openapi.growatt.com/login",
@@ -35,7 +36,7 @@ func (m *MockHttpClient) OnLogin(account string, password string, err error) *mo
 		call = call.Run(
 			func(args mock.Arguments) {
 				responseBody := args.Get(2).(*GrowattResult)
-				*responseBody = GrowattResult{}
+				*responseBody = result
 			},
 		)
 	}
@@ -56,6 +57,145 @@ func (m *MockHttpClient) OnGetPlantList(result []GrowattPlant, err error) *mock.
 		call = call.Run(
 			func(args mock.Arguments) {
 				responseBody := args.Get(2).(*[]GrowattPlant)
+				*responseBody = result
+			},
+		)
+	}
+
+	return call.Return(err)
+}
+
+func (m *MockHttpClient) OnGetPlantDevices(plantId string, result GrowattPlantDevices, err error) *mock.Call {
+	call := m.On(
+		"postForm",
+		"https://openapi.growatt.com/panel/getDevicesByPlantList",
+		url.Values{
+			"plantId":  {plantId},
+			"currPage": {"1"},
+		},
+		&GrowattPlantDevices{},
+	)
+
+	if err == nil {
+		call = call.Run(
+			func(args mock.Arguments) {
+				responseBody := args.Get(2).(*GrowattPlantDevices)
+				*responseBody = result
+			},
+		)
+	}
+
+	return call.Return(err)
+}
+
+func (m *MockHttpClient) OnGetNoahList(plantId int, result GrowattNoahList, err error) *mock.Call {
+	call := m.On(
+		"postForm",
+		"https://openapi.growatt.com/device/getNoahList",
+		url.Values{
+			"plantId":  {fmt.Sprintf("%d", plantId)},
+			"currPage": {"1"},
+		},
+		&GrowattNoahList{},
+	)
+
+	if err == nil {
+		call = call.Run(
+			func(args mock.Arguments) {
+				responseBody := args.Get(2).(*GrowattNoahList)
+				*responseBody = result
+			},
+		)
+	}
+
+	return call.Return(err)
+}
+
+func (m *MockHttpClient) OnGetNoahDetails(plantId int, serial string, result GrowattNoahList, err error) *mock.Call {
+	call := m.On(
+		"postForm",
+		"https://openapi.growatt.com/device/getNoahList",
+		url.Values{
+			"plantId":  {fmt.Sprintf("%d", plantId)},
+			"deviceSn": {serial},
+			"currPage": {"1"},
+		},
+		&GrowattNoahList{},
+	)
+
+	if err == nil {
+		call = call.Run(
+			func(args mock.Arguments) {
+				responseBody := args.Get(2).(*GrowattNoahList)
+				*responseBody = result
+			},
+		)
+	}
+
+	return call.Return(err)
+}
+
+func (m *MockHttpClient) OnGetNoahHistory(serial string, startDate string, endDate string, result GrowattNoahHistory, err error) *mock.Call {
+	call := m.On(
+		"postForm",
+		"https://openapi.growatt.com/device/getNoahHistory",
+		url.Values{
+			"deviceSn":  {serial},
+			"start":     {"0"},
+			"startDate": {startDate},
+			"endDate":   {endDate},
+		},
+		&GrowattNoahHistory{},
+	)
+
+	if err == nil {
+		call = call.Run(
+			func(args mock.Arguments) {
+				responseBody := args.Get(2).(*GrowattNoahHistory)
+				*responseBody = result
+			},
+		)
+	}
+
+	return call.Return(err)
+}
+
+func (m *MockHttpClient) OnGetNoahStatus(plantId int, serial string, result GrowattNoahStatus, err error) *mock.Call {
+	call := m.On(
+		"postForm",
+		fmt.Sprintf("https://openapi.growatt.com/panel/noah/getNoahStatusData?plantId=%d", plantId),
+		url.Values{
+			"deviceSn": {serial},
+		},
+		&GrowattNoahStatus{},
+	)
+
+	if err == nil {
+		call = call.Run(
+			func(args mock.Arguments) {
+				responseBody := args.Get(2).(*GrowattNoahStatus)
+				*responseBody = result
+			},
+		)
+	}
+
+	return call.Return(err)
+}
+
+func (m *MockHttpClient) OnGetNoahTotals(plantId int, serial string, result GrowattNoahTotals, err error) *mock.Call {
+	call := m.On(
+		"postForm",
+		fmt.Sprintf("https://openapi.growatt.com/panel/noah/getNoahTotalData?plantId=%d", plantId),
+		url.Values{
+			"deviceSn": {serial},
+		},
+		&GrowattNoahTotals{},
+	)
+
+	if err == nil {
+		call = call.Run(
+			func(args mock.Arguments) {
+				responseBody := args.Get(2).(*GrowattNoahTotals)
 				*responseBody = result
 			},
 		)
